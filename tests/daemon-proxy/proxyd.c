e7b0a512719c512835888a27f743d9cbc4deb9b9
rradjabi <rradjabi@gmail.com>
2019-02-01 10:45:11 -0800

    Updates daemon and gives feedback when packets are returned by IOCTL.

--- tests/daemon-proxy/proxyd.c
+++ tests/daemon-proxy/proxyd.c
@@ -19,21 +19,28 @@
 int main(void)
 {
     // add echo 1 > sysfs/mailbox...ctrl to enable sw channel in mgmt
-
+    //
     int mUserHandle = 0;
     mUserHandle = open("/dev/dri/renderD129", O_RDWR);
     if( mUserHandle < 0 )
         printf( "Error openning /dev/dri/renderD129\n" );
     
+    int ret = -1;
     uint64_t flags = 0;
     void *ptr;
     uint64_t *buffer;
     buffer = malloc( 64 * 2 * sizeof(uint64_t) );
     ptr = buffer;
     struct drm_xocl_sw_mailbox args = { flags, ptr, 64 };
-    //struct drm_xocl_sw_mailbox args = { 0, 0, 0 };
-    printf( "ioctl return: %d\n", ioctl(mUserHandle, DRM_IOCTL_XOCL_SW_MAILBOX, &args) );
-    printf( "Errno: %s\n", strerror(errno));
+
+    printf( "[XOCL->XCLMGMT Intercept ON]\n" );
+
+    for( ;; ) {
+        ret = ioctl(mUserHandle, DRM_IOCTL_XOCL_SW_MAILBOX, &args);
+        printf( "[..]\n" );
+        if( ret < 0 )
+            printf( "Errno: %s\n", strerror(errno));
+    }
 
     return 0;
 }
